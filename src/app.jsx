c5edcde7eaaea4813252e69c02318f1dba61a5f5
Clara Oliveira <claraoliveira.8351@gmail.com>
2024-05-20 19:10:54 -0300

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h1">Bem-vinda, Clara Beatriz!</Typography>
+      <Typography variant="subtitle1">
+        Projeto de Clara Beatriz - Terceiro Período - Programação Frontend
+      </Typography>
+      <Button variant="contained" color="primary">
+        Botão de Exemplo
+      </Button>
+      // Atividade de márcio
+    </div>
+  );
+}
+
+export default App;
